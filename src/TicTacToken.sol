f33bfd9927e1976032867d8e262e1f9997a60090
Ian Gagorik <ian.gagorik@gmail.com>
2021-11-10 19:16:29 -0800

    add a board

--- src/TicTacToken.sol
+++ src/TicTacToken.sol
@@ -2,4 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract TicTacToken {
+    uint256[9] public board;
+
+    function getBoard() public view returns (uint256[9] memory) {
+        return board;
+    }
 }
--- src/test/TicTacToken.t.sol
+++ src/test/TicTacToken.t.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.0;
 import "./utils/TicTacTokenTest.sol";
 
 contract TestTTT is TicTacTokenTest {
-    function testNothing() public {
-        assertEq(uint8(1), 1);
+    function test_has_empty_board() public {
+        for (uint256 i = 0; i < 9; i++) {
+            assertEq(ttt.board(i), 0);
+        }
+    }
+
+    function test_get_board() public {
+        uint256[9] memory expectedBoard = [uint256(0), 0, 0, 0, 0, 0, 0, 0, 0];
+
+        uint256[9] memory actualBoard = ttt.getBoard();
+
+        for (uint256 i = 0; i < 9; i++) {
+            assertEq(actualBoard[i], expectedBoard[i]);
+        }
     }
 }
