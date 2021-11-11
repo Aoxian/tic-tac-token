4c008332b4136d1beacd0de05e0dea66b2d50f2b
Ian Gagorik <ian.gagorik@gmail.com>
2021-11-10 20:07:25 -0800

    add ability to mark a board

--- src/TicTacToken.sol
+++ src/TicTacToken.sol
@@ -2,9 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract TicTacToken {
-    uint256[9] public board;
+    string[9] public board;
 
-    function getBoard() public view returns (uint256[9] memory) {
+    function getBoard() public view returns (string[9] memory) {
         return board;
+    }
+
+    function markSpace(uint256 _space, string calldata _mark) public {
+        require(_validMark(_mark), "Invalid Mark");
+        require(_emptySpace(_space), "Already Marked");
+        board[_space] = _mark;
+    }
+
+    function _emptySpace(uint256 _space) internal view returns (bool) {
+        return _compareString(board[_space], "");
+    }
+
+    function _validMark(string calldata _mark) internal pure returns (bool) {
+        return _compareString(_mark, "X") || _compareString(_mark, "O");
+    }
+
+    function _compareString(string memory a, string memory b)
+        internal
+        pure
+        returns (bool)
+    {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
 }
--- src/test/TicTacToken.t.sol
+++ src/test/TicTacToken.t.sol
@@ -6,17 +6,42 @@ import "./utils/TicTacTokenTest.sol";
 contract TestTTT is TicTacTokenTest {
     function test_has_empty_board() public {
         for (uint256 i = 0; i < 9; i++) {
-            assertEq(ttt.board(i), 0);
+            assertEq(ttt.board(i), "");
         }
     }
 
     function test_get_board() public {
-        uint256[9] memory expectedBoard = [uint256(0), 0, 0, 0, 0, 0, 0, 0, 0];
+        string[9] memory expectedBoard = ["", "", "", "", "", "", "", "", ""];
 
-        uint256[9] memory actualBoard = ttt.getBoard();
+        string[9] memory actualBoard = ttt.getBoard();
 
         for (uint256 i = 0; i < 9; i++) {
             assertEq(actualBoard[i], expectedBoard[i]);
         }
+    }
+
+    function test_can_mark_space_with_X(uint256 space) public {
+        if (space > 8) return;
+        ttt.markSpace(space, "X");
+
+        assertEq(ttt.board(space), "X");
+    }
+
+    function test_can_mark_space_with_O(uint256 space) public {
+        if (space > 8) return;
+        ttt.markSpace(space, "O");
+
+        assertEq(ttt.board(space), "O");
+    }
+
+    function testFail_cannot_mark_space_with_Z() public {
+        ttt.markSpace(0, "Z");
+
+        assertEq(ttt.board(0), "Z");
+    }
+
+    function testFail_cannot_overwrite_marked_space() public {
+        ttt.markSpace(0, "X");
+        ttt.markSpace(0, "O");
     }
 }
