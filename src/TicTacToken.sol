ad3b92e5cbee422cf86028484824e29beb46e90b
Ian Gagorik <ian.gagorik@gmail.com>
2021-11-10 21:12:16 -0800

    update board to use integer values

--- src/TicTacToken.sol
+++ src/TicTacToken.sol
@@ -2,31 +2,27 @@
 pragma solidity ^0.8.0;
 
 contract TicTacToken {
-    string[9] public board;
+    uint256[9] public board;
 
-    function getBoard() public view returns (string[9] memory) {
+    uint256 public constant EMPTY = 0;
+    uint256 public constant X = 1;
+    uint256 public constant O = 2;
+
+    function getBoard() public view returns (uint256[9] memory) {
         return board;
     }
 
-    function markSpace(uint256 _space, string calldata _mark) public {
+    function markSpace(uint256 _space, uint256 _mark) public {
         require(_validMark(_mark), "Invalid Mark");
         require(_emptySpace(_space), "Already Marked");
         board[_space] = _mark;
     }
 
     function _emptySpace(uint256 _space) internal view returns (bool) {
-        return _compareString(board[_space], "");
+        return board[_space] == 0;
     }
 
-    function _validMark(string calldata _mark) internal pure returns (bool) {
-        return _compareString(_mark, "X") || _compareString(_mark, "O");
-    }
-
-    function _compareString(string memory a, string memory b)
-        internal
-        pure
-        returns (bool)
-    {
-        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    function _validMark(uint256 _mark) internal pure returns (bool) {
+        return _mark == X || _mark == O;
     }
 }
--- src/test/TicTacToken.t.sol
+++ src/test/TicTacToken.t.sol
@@ -4,16 +4,37 @@ pragma solidity ^0.8.0;
 import "./utils/TicTacTokenTest.sol";
 
 contract TestTTT is TicTacTokenTest {
+    uint256 internal EMPTY;
+    uint256 internal X;
+    uint256 internal O;
+
+    function setUp() public override {
+        super.setUp();
+        EMPTY = ttt.EMPTY();
+        X = ttt.X();
+        O = ttt.O();
+    }
+
     function test_has_empty_board() public {
         for (uint256 i = 0; i < 9; i++) {
-            assertEq(ttt.board(i), "");
+            assertEq(ttt.board(i), EMPTY);
         }
     }
 
     function test_get_board() public {
-        string[9] memory expectedBoard = ["", "", "", "", "", "", "", "", ""];
+        uint256[9] memory expectedBoard = [
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY
+        ];
 
-        string[9] memory actualBoard = ttt.getBoard();
+        uint256[9] memory actualBoard = ttt.getBoard();
 
         for (uint256 i = 0; i < 9; i++) {
             assertEq(actualBoard[i], expectedBoard[i]);
@@ -22,26 +43,26 @@ contract TestTTT is TicTacTokenTest {
 
     function test_can_mark_space_with_X(uint256 space) public {
         if (space > 8) return;
-        ttt.markSpace(space, "X");
+        ttt.markSpace(space, X);
 
-        assertEq(ttt.board(space), "X");
+        assertEq(ttt.board(space), X);
     }
 
     function test_can_mark_space_with_O(uint256 space) public {
         if (space > 8) return;
-        ttt.markSpace(space, "O");
+        ttt.markSpace(space, O);
 
-        assertEq(ttt.board(space), "O");
+        assertEq(ttt.board(space), O);
     }
 
-    function testFail_cannot_mark_space_with_Z() public {
-        ttt.markSpace(0, "Z");
+    function testFail_cannot_mark_space_with_other_symbol() public {
+        ttt.markSpace(0, 3);
 
-        assertEq(ttt.board(0), "Z");
+        assertEq(ttt.board(0), 3);
     }
 
     function testFail_cannot_overwrite_marked_space() public {
-        ttt.markSpace(0, "X");
-        ttt.markSpace(0, "O");
+        ttt.markSpace(0, X);
+        ttt.markSpace(0, O);
     }
 }
